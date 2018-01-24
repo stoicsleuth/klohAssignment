87d7750015a8608b36a96089b3a3d203234ad912
stoicsleuth <tarashnkr321@gmail.com>
2018-01-24 12:45:33 +0530

    0.0.3

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
     res.render('notFound');
 });
 
-app.set('port', 7777);
+app.set('port', process.env.PORT||7777);
 const server = app.listen(app.get('port'), () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
